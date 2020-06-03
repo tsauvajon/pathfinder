525355f3d6d4a011817a05a5fef7f2a40cdddfc0
Thomas Sauvajon <thomas@sauvajon.tech>
2020-06-03 19:43:31 +1000

    Cell component

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,184 @@
-// extern crate console_error_panic_hook;
+// mod node;
 
-mod grid;
-
-// use std::panic;
-
-use rand::Rng;
+use std::time::Duration;
 
 use wasm_bindgen::prelude::*;
 use yew::prelude::*;
+use yew::services::{ConsoleService, IntervalService};
 
-use grid::{Grid, GridMethods};
+// use node::{Cell, Node};
+
+// extern crate rand;
+
+// use rand::Rng;
+use std::fmt;
+
+use yew::prelude::*;
+
+// fn main() {
+//     let grid = Grid::create(15, 15);
+//
+//     let mut rng = rand::thread_rng();
+//     for _ in 0..20 {
+//         let mut g = grid.clone();
+//
+//         let sx: usize = rng.gen_range(0, g.width());
+//         let sy: usize = rng.gen_range(0, g.height());
+//         let tx: usize = rng.gen_range(0, g.width());
+//         let ty: usize = rng.gen_range(0, g.height());
+//
+//         g[sx][sy].set_start();
+//         g[tx][ty].set_target();
+//         solve(g);
+//     }
+// }
+
+#[derive(fmt::Debug, Copy, Clone, PartialEq)]
+pub enum State {
+    Empty,
+    Start,
+    Visited,
+    Path,
+    Target,
+}
+
+impl fmt::Display for State {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            State::Empty => write!(f, "."),
+            State::Visited => write!(f, "_"),
+            State::Start => write!(f, "O"),
+            State::Path => write!(f, "+"),
+            State::Target => write!(f, "x"),
+        }
+    }
+}
+
+#[derive(fmt::Debug, Clone)]
+pub struct Node {
+    x: usize,
+    y: usize,
+    state: State,
+    active: bool,
+    parent_coords: Option<(usize, usize)>,
+}
+
+impl Node {
+    fn new(x: usize, y: usize) -> Node {
+        Node {
+            x,
+            y,
+            state: State::Empty,
+            active: false,
+            parent_coords: None,
+            // link: ComponentLink<Self>::new,
+        }
+    }
+
+    fn is_empty(&self) -> bool {
+        match self.state {
+            State::Empty => true,
+            _ => false,
+        }
+    }
+
+    fn is_target(&self) -> bool {
+        match self.state {
+            State::Target => true,
+            _ => false,
+        }
+    }
+
+    fn is_visited(&self) -> bool {
+        match self.state {
+            State::Visited => true,
+            _ => false,
+        }
+    }
+
+    pub fn set_start(&mut self) {
+        self.state = State::Start;
+        self.active = true;
+    }
+
+    pub fn set_target(&mut self) {
+        self.state = State::Target;
+    }
+
+    fn visit(&mut self, p_x: usize, p_y: usize) {
+        match self.state {
+            State::Empty => (),
+            _ => return,
+        }
+        self.state = State::Visited;
+        self.active = true;
+        self.parent_coords = Some((p_x, p_y))
+    }
+
+    fn mark_path(&mut self) -> Option<(usize, usize)> {
+        if self.is_visited() {
+            self.state = State::Path;
+            self.parent_coords
+        } else {
+            None
+        }
+    }
+}
+
+pub struct Cell {
+    state: State,
+    active: bool,
+    link: ComponentLink<Self>,
+}
+
+#[derive(Properties, Clone)]
+pub struct Props {
+    pub state: State,
+    pub active: bool,
+}
+
+pub enum CellMsg {
+    Select,
+}
+
+impl Component for Cell {
+    type Message = CellMsg;
+    type Properties = Props;
+
+    fn create(props: Self::Properties, link: ComponentLink<Self>) -> Self {
+        Self {
+            state: props.state,
+            active: props.active,
+            link,
+        }
+    }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            CellMsg::Select => self.state = State::Visited,
+        }
+        true
+    }
+
+    fn change(&mut self, props: Self::Properties) -> ShouldRender {
+        // Should only return "true" if new properties are different to
+        // previously received properties.
+        self.active == props.active && self.state == props.state
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <button onclick=self.link.callback(|_| CellMsg::Select)>
+                { format!("{}", self.state) }
+            </button>
+        }
+    }
+}
+// impl fmt::Display for Node {
+//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+//         write!(f, "({}, {})", self.x, self.y)
+//     }
+// }
 
 struct Model {
     link: ComponentLink<Self>,
@@ -18,8 +187,8 @@ struct Model {
 }
 
 enum Msg {
-    AddOne,
-    Select(usize, usize),
+    Next,
+    // Select(usize, usize),
 }
 
 impl Component for Model {
@@ -27,15 +196,11 @@ impl Component for Model {
     type Properties = ();
 
     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
-        // console_error_panic_hook::set_once();
-
         let mut g = Grid::create(20, 20);
-        // let mut rng = rand::thread_rng();
-        //
-        // let sx: usize = rng.gen_range(0, g.width());
-        // let sy: usize = rng.gen_range(0, g.height());
-        // let tx: usize = rng.gen_range(0, g.width());
-        // let ty: usize = rng.gen_range(0, g.height());
+
+        let callback = link.callback(|_| Msg::Next);
+        let mut interval = IntervalService::new();
+        let handle = interval.spawn(Duration::from_millis(200), callback);
 
         g[3][3].set_start();
         g[14][14].set_target();
@@ -47,11 +212,19 @@ impl Component for Model {
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        let mut console = ConsoleService::new();
+        console.log("hello");
         match msg {
-            Msg::AddOne => self.value += 1,
-            Msg::Select(x, y) => (),
+            Msg::Next => {
+                match step(self.grid.clone()) {
+                    Some(next) => {
+                        self.grid = next;
+                        true
+                    }
+                    None => false, // stop callbacks
+                }
+            }
         }
-        true
     }
 
     fn change(&mut self, _props: Self::Properties) -> ShouldRender {
@@ -64,13 +237,7 @@ impl Component for Model {
     fn view(&self) -> Html {
         html! {
             <div>
-                // <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
-                <p>{ self.value }</p>
-                {
-                    self.grid.render()
-                }
-
-                // <button onclick=self.link.callback(|_| Msg:Select(4, 4))></button>
+                { self.grid.render() }
             </div>
         }
     }
@@ -78,6 +245,159 @@ impl Component for Model {
 
 #[wasm_bindgen(start)]
 pub fn run_app() {
-    // panic::set_hook(Box::new(console_error_panic_hook::hook));
     App::<Model>::new().mount_to_body();
+}
+
+///////////////
+
+pub type Grid = Vec<Vec<Node>>;
+
+pub trait GridMethods {
+    fn create(width: usize, height: usize) -> Self;
+    fn print(&self);
+
+    fn neighbours(&self, i: usize, j: usize) -> Vec<Node>;
+
+    fn width(&self) -> usize;
+    fn height(&self) -> usize;
+
+    fn render(&self) -> Html;
+}
+
+impl GridMethods for Grid {
+    fn create(width: usize, height: usize) -> Grid {
+        let mut matrix: Grid = vec![];
+        for i in 0..width {
+            let mut row: Vec<Node> = vec![];
+            for j in 0..height {
+                row.push(Node::new(i, j));
+            }
+            matrix.push(row);
+        }
+        matrix
+    }
+
+    fn print(&self) {
+        print!("\x1B[2J");
+        for r in self {
+            for n in r {
+                print!("({}) ", n.state);
+            }
+            println!();
+        }
+    }
+
+    fn neighbours(&self, i: usize, j: usize) -> Vec<Node> {
+        let mut nb: Vec<Node> = vec![];
+
+        let possible_nb: Vec<(isize, isize)> = vec![(-1, 0), (1, 0), (0, -1), (0, 1)];
+
+        for p in possible_nb {
+            let nb_i = (i as isize + p.0) as usize;
+            let nb_j = (j as isize + p.1) as usize;
+            if (0..self.width() as usize).contains(&nb_i)
+                && (0..self.height() as usize).contains(&nb_j)
+            {
+                nb.push(self[nb_i][nb_j].clone());
+            }
+        }
+
+        nb
+    }
+
+    fn width(&self) -> usize {
+        self.len()
+    }
+
+    fn height(&self) -> usize {
+        self[0].len()
+    }
+
+    fn render(&self) -> Html {
+        html! {
+            // <div>{ "hello" }</div>
+            for self.iter().map(|mut row| {
+                html! {
+                    <div>
+                        {
+                            for row.iter().map(|n| {
+                            // "a"
+                                html! { <Cell state=n.state active=n.active /> }
+                                // n.render()
+                            })
+                        }
+                    // <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
+                    </div>
+                }
+            })
+        }
+    }
+}
+
+/// update grid by one step
+/// actually for now it's one loop not 1 step
+pub fn step_mut(mut g: Grid) -> bool {
+    match step(g.clone()) {
+        Some(next) => {
+            g = next;
+            true
+        }
+        None => false,
+    }
+}
+
+fn step(g: Grid) -> Option<Grid> {
+    // todo: keep track of active nodes to skip lots of browsing
+    // and also if active.len() == 0 that means there's no path
+    let mut next = g.clone();
+    for (i, r) in g.iter().enumerate() {
+        for (j, n) in r.iter().enumerate() {
+            if !n.active {
+                continue;
+            };
+
+            next[n.x][n.y].active = false;
+
+            let nbs = g.neighbours(n.x, n.y);
+            for nb in nbs {
+                if nb.is_target() {
+                    let end = mark_path(next, n);
+                    // end.print();
+                    return None;
+                } else if nb.is_empty() {
+                    next[nb.x][nb.y].visit(i, j);
+                }
+            }
+        }
+    }
+
+    Some(next)
+}
+
+fn mark_path(g: Grid, n: &Node) -> Grid {
+    let mut next = g;
+    let mut x = n.x;
+    let mut y = n.y;
+
+    loop {
+        match next[x][y].mark_path() {
+            Some((nx, ny)) => {
+                x = nx;
+                y = ny;
+            }
+            None => break,
+        }
+    }
+
+    next
+}
+
+pub fn solve(g: Grid) {
+    let mut g = g;
+    loop {
+        g = match step(g) {
+            Some(next) => next,
+            None => return,
+        };
+    }
 }
